0f8d6ebf13194db601b98caf7ac54d60e16c0088
hocarm <hocarm.org@gmail.com>
2016-11-05 00:11:14 +0700

    Init
    Khởi tạo project

---
+++ Out/main.d
@@ -0,0 +1,6 @@
+.\out\main.o: main.c
+.\out\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\out\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\out\main.o: C:\ti\TivaWare_C_Series-2.1.2.111\inc/hw_memmap.h
+.\out\main.o: C:\ti\TivaWare_C_Series-2.1.2.111\driverlib/gpio.h
+.\out\main.o: C:\ti\TivaWare_C_Series-2.1.2.111\driverlib/sysctl.h
